53ae8f5ff95dde7a06bcd82cfb20c64d42ee8b4f
AhmedELsayedMohamed <ahmed771994@gmail.com>
2022-03-27 20:34:42 +0200

    Initial commit ,  API Skeleton
    in this commit, we build the skeleton for the API of the Project  we added the controller, models and build the Migration for the Database

---
+++ API/Models/Data/DataContext.cs
@@ -0,0 +1,15 @@
+
+
+using API.Models.Entites;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
